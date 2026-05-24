9cadf03cb3fe51e0afc09f47450ebb29e89c5d57
Eng. Ammar A. Al-Shameri <106429113+ammarCEN@users.noreply.github.com>
2026-05-24 14:27:25 +0300

    Solve theme conflict

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -1,47 +1,43 @@
 "use client"
 
-import { useModeAnimation } from 'react-theme-switch-animation'
-import { Moon, Sun } from "lucide-react"
+import { useEffect, useState } from "react";
+import { useModeAnimation } from "react-theme-switch-animation";
+import { Moon, Sun } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils";
-import { useTheme } from 'next-themes';
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 
 export function ModeToggle() {
-    const { setTheme } = useTheme();
-    const { ref, toggleSwitchTheme, isDarkMode } = useModeAnimation();
+    const [mounted, setMounted] = useState(false);
+
+    const { setTheme, resolvedTheme } = useTheme();
+
+    const { ref, toggleSwitchTheme } = useModeAnimation();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+
+    const isDarkMode = resolvedTheme === "dark";
 
     const handleToggleTheme = async () => {
         await toggleSwitchTheme();
+
         setTheme(isDarkMode ? "light" : "dark");
-    }
+    };
 
     return (
         <Button
-            variant='outline'
-            size='icon'
+            variant="outline"
+            size="icon"
             ref={ref}
             onClick={handleToggleTheme}
         >
-
-            {isDarkMode ? (
-                <Moon
-                // className={cn(
-                //     "absolute h-5 w-5",
-                //     "transition-transform transition-opacity duration-500 ease-in-out",
-                // )}
-                />
-            ) : (
-                <Sun
-                // className={cn(
-                //     "absolute h-[1.5rem] w-[1.3rem]",
-                //     "transition-transform transition-opacity duration-500 ease-in-out",
-                // )}
-                />
-            )}
-
+            {isDarkMode ? <Moon /> : <Sun />}
 
             <span className="sr-only">Toggle theme</span>
         </Button>
-    )
+    );
 }
